d756b568f4d36cf61f1eecb23e1f8af6993e354d
Edyta Boczarska <28872@student.pwsztar.edu.pl>
2023-05-04 17:03:49 +0200

    lab7

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.MONGODB_URI || '',
+  databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://scoobychrupka:7qt2aic30C9ytqWg@cluster0.2a2ibrs.mongodb.net/?retryWrites=true&w=majority',
   JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 
